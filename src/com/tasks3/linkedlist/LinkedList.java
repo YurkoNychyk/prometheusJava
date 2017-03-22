3306701e0cdc5f6134ca96e127f35a0f8e8f4830
yurko.nychik <yurko.nychik@gmail.com>
2017-03-22 20:46:35 +0200

    changed class type of a 'Node' class to 'nested'  new branch 'tictactoe' -- example classes loaded

--- src/com/tasks3/linkedlist/LinkedList.java
+++ src/com/tasks3/linkedlist/LinkedList.java
@@ -5,6 +5,27 @@ package com.tasks3.linkedlist;
  */
 
 public class LinkedList {
+    static class Node{
+        private Node next;
+        private Integer data;
+
+        public Node() {
+        }
+
+        public Node getNext() {
+            return next;
+        }
+        public void setNext(Node next) {
+            this.next = next;
+        }
+        public Integer getData() {
+            return data;
+        }
+        public void setData(Integer data) {
+            this.data = data;
+        }
+    }
+
     private Node tail;
     private Node head;
     private int size = 0;
